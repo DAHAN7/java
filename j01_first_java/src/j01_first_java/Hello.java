10e75df80318117c8858b5e59a610a2855b8d818
DAHAN7 <ekgks4843@gmail.com>
2024-04-29 14:15:33 +0900

    first commit

--- j01_first_java/src/j01_first_java/Hello.java
+++
@@ -1,8 +0,0 @@
-package j01_first_java;
-
-public class Hello {
-    public static void main(String[] args) {
-    	System.out.println();
-        System.out.println("Hello world!!!");
-    }
-}
---
+++ j02_operators/src/control_statement/IfSwitchExample.java
@@ -0,0 +1,5 @@
+package control_statement;
+
+public class IfSwitchExample {
+
+}
