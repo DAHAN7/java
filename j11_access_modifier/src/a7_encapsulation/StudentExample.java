115c3c67e0cd9e71cbfcdf68b5ea299c468600b9
DAHAN7 <ekgks4843@gmail.com>
2024-05-16 15:33:42 +0900

    first commit

--- j11_access_modifier/src/a7_encapsulation/StudentExample.java
+++ j11_access_modifier/src/a7_encapsulation/StudentExample.java
@@ -5,6 +5,17 @@ public class StudentExample {
 	public static void main(String[] args) {
 		System.out.println(Student.MIN_GRADE);
 		System.out.println(Student.MAX_GRADE);
+		
+		Student choi = new Student();
+		choi.name = "최기근";
+		
+		choi.age  = -20;
 	}
 
 }
+
+
+
+
+
+
